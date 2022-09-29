a6ea65b7153877885c753288f6d9ac4dab10acbb
Carlossiii <cv5673@gmail.com>
2022-09-29 16:40:00 -0300

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Carlos Vinicius on 29/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
